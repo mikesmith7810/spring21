a7c2d8ba425e38998c210cf6602c6dfd4e5cb4f8
mikesmith-createfuture <mike.smith@xdesign.com>
2024-09-23 10:58:12 +0100

    controller 2

--- core/src/main/java/com/example/service/HelloService.java
+++ core/src/main/java/com/example/service/HelloService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloService {
     public String giveAnswer(final String name) {
-        return "Hello " + name;
+        return "Hello and greetings to " + name;
     }
 }
