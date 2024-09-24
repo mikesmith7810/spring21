46a35f5de0c5c3e64a7ad2c0eacee8d353da49b7
mikesmith-createfuture <mike.smith@xdesign.com>
2024-09-24 08:41:06 +0100

    docker

--- src/main/java/com/example/Spring21Application.java
+++ src/main/java/com/example/Spring21Application.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("!dev")
 public class Spring21Application {
 
     public static void main(String[] args) {
