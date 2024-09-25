b2b032c6886038bcdbff79555d88abe7b0c04f3d
mikesmith-createfuture <mike.smith@xdesign.com>
2024-09-25 11:02:20 +0100

    added logging

--- core/src/main/java/com/example/service/HelloService.java
+++ core/src/main/java/com/example/service/HelloService.java
@@ -1,10 +1,18 @@
 package com.example.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class HelloService {
+
     public String giveAnswer(final String name) {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
         return "Hello and greetings to " + name;
     }
 }
--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -2,13 +2,15 @@ package com.example;
 
 import com.example.model.PersonalDetails;
 import com.example.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
-
+    
     private final HelloService helloService;
 
     public HelloController(final HelloService helloService) {
@@ -17,7 +19,11 @@ public class HelloController {
 
     @GetMapping("/")
     public String index(@RequestBody final PersonalDetails personalDetails) {
-
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
         return helloService.giveAnswer(personalDetails.name());
     }
 
